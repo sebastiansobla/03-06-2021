5c0329f8b2bd5a8b0cc0e68b8a2c5685d85d6786
Sebastian Soto Blanco <sebastiansobla@hotmail.com>
2021-06-03 12:56:08 -0500

    03062021

---
+++ data/data.js
@@ -0,0 +1,184 @@
+ export  const data=
+[
+    {
+        "id": 1,
+        "name": "dc-batman",
+        "superhero": "Batman",
+        "publisher": "DC Comics",
+        "alter_ego": "Bruce Wayne",
+        "first_appearance": "Detective Comics #27",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-batman.jpg"
+        
+    },
+    {
+        "id": 2,
+        "name": "dc-superman",
+        "superhero": "Superman",
+        "publisher": "DC Comics",
+        "alter_ego": "Kal-El",
+        "first_appearance": "Action Comics #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-superman.jpg"
+    },
+    {
+        "id": 3,
+        "name": "dc-flash",
+        "superhero": "Flash",
+        "publisher": "DC Comics",
+        "alter_ego": "Jay Garrick",
+        "first_appearance": "Flash Comics #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-flash.jpg"
+    },
+    {
+        "id": 4,
+        "name": "dc-green",
+        "superhero": "Green Lantern",
+        "publisher": "DC Comics",
+        "alter_ego": "Alan Scott",
+        "first_appearance": "All-American Comics #16",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-green.jpg"
+    },
+    {
+        "id": 5,
+        "name": "dc-arrow",
+        "superhero": "Green Arrow",
+        "publisher": "DC Comics",
+        "alter_ego": "Oliver Queen",
+        "first_appearance": "More Fun Comics #73",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-arrow.jpg"
+    },
+    {
+        "id": 6,
+        "name": "dc-wonder",
+        "superhero": "Wonder Woman",
+        "publisher": "DC Comics",
+        "alter_ego": "Princess Diana",
+        "first_appearance": "All Star Comics #8",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-wonder.jpg"
+    },
+    {
+        "id": 7,
+        "name": "dc-martian",
+        "superhero": "Martian Manhunter",
+        "publisher": "DC Comics",
+        "alter_ego": "J\"onn J\"onzz",
+        "first_appearance": "Detective Comics #225",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-martian.jpg"
+    },
+    {
+        "id": 8,
+        "name": "dc-robin",
+        "superhero": "Robin/Nightwing",
+        "publisher": "DC Comics",
+        "alter_ego": "Dick Grayson",
+        "first_appearance": "Detective Comics #38",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-robin.jpg"
+    },
+    {
+        "id": 9,
+        "name": "dc-blue",
+        "superhero": "Blue Beetle",
+        "publisher": "DC Comics",
+        "alter_ego": "Dan Garret",
+        "first_appearance": "Mystery Men Comics #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-blue.jpg"
+    },
+    {
+        "id": 10,
+        "name": "dc-black",
+        "superhero": "Black Canary",
+        "publisher": "DC Comics",
+        "alter_ego": "Dinah Drake",
+        "first_appearance": "Flash Comics #86",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/dc-black.jpg"
+    },
+    {
+        "id": 11,
+        "name": "marvel-spider",
+        "superhero": "Spider Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Peter Parker",
+        "first_appearance": "Amazing Fantasy #15",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-spider.jpg"
+    },
+    {
+        "id": 12,
+        "name": "marvel-captain",
+        "superhero": "Captain America",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Steve Rogers",
+        "first_appearance": "Captain America Comics #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-captain.jpg"
+    },
+    {
+        "id": 13,
+        "name": "marvel-iron",
+        "superhero": "Iron Man",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Tony Stark",
+        "first_appearance": "Tales of Suspense #39",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-iron.jpg"
+    },
+    {
+        "id": 14,
+        "name": "marvel-thor",
+        "superhero": "Thor",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Thor Odinson",
+        "first_appearance": "Journey into Myster #83",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-thor.jpg"
+    },
+    {
+        "id": 15,
+        "name": "marvel-hulk",
+        "superhero": "Hulk",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Bruce Banner",
+        "first_appearance": "The Incredible Hulk #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-hulk.jpg"
+    },
+    {
+        "id": 16,
+        "name": "marvel-wolverine",
+        "superhero": "Wolverine",
+        "publisher": "Marvel Comics",
+        "alter_ego": "James Howlett",
+        "first_appearance": "The Incredible Hulk #180",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-wolverine.jpg"
+    },
+    {
+        "id": 17,
+        "name": "marvel-daredevil",
+        "superhero": "Daredevil",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Matthew Michael Murdock",
+        "first_appearance": "Daredevil #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-daredevil.jpg"
+    },
+    {
+        "id": 18,
+        "name": "marvel-hawkeye",
+        "superhero": "Hawkeye",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Clinton Francis Barton",
+        "first_appearance": "Tales of Suspense #57",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-hawkeye.jpg"
+    },
+    {
+        "id": 19,
+        "name": "marvel-cyclops",
+        "superhero": "Cyclops",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Scott Summers",
+        "first_appearance": "X-Men #1",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-cyclops.jpg"
+    },
+    {
+        "id": 20,
+        "name": "marvel-silver",
+        "superhero": "Silver Surfer",
+        "publisher": "Marvel Comics",
+        "alter_ego": "Norrin Radd",
+        "first_appearance": "The Fantastic Four #48",
+        "image": "https://raw.githubusercontent.com/jennymontoya1001/heroes/main/marvel-silver.jpg"
+    }
+]
